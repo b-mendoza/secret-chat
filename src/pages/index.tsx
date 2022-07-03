ca4b0662c78944baa5eec2ea214a2f42588e9cbc
Bryan Mendoza <bmendoza.dev@hotmail.com>
2022-07-02 21:30:58 -0600

    feat: update `IndexPage` setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,11 @@
-const IndexPage = () => <h1>Hello World</h1>;
+import { trpc } from '@/utils/trpc';
+
+const IndexPage = () => {
+  const helloWorld = trpc.useQuery(['helloWorld', { text: 'bryan' }]);
+
+  const { data } = helloWorld;
+
+  return <h1>{JSON.stringify(data, null, 2)}</h1>;
+};
 
 export default IndexPage;
